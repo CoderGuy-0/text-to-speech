64e82d77b02bd3214a39504dcc9e99bd206dd47a
Karri <Karri.helokumpu@hotmail.com>
2022-10-11 17:47:51 +0300

    First Commit

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, TextInput , Text, Button } from 'react-native';
+import * as Speech from 'expo-speech';
+
 
 export default function App() {
+
+  const [value1, setValue] = React.useState('')
+
+  const speak = () => {
+    const thingToSay = value1;
+    Speech.speak(thingToSay);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+      style= {{borderColor: 'gray', borderWidth: 1, width: 200}}
+      onChangeText={(value1) => setValue(value1)}
+      placeholder={'Insert text you want to hear'}
+      value={value1}
+      />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
